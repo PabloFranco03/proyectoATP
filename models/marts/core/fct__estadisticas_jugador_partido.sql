bd6441fd401debd3ef6c805a9cab9c301ca21542
Pablo Franco <alumno26git@gmail.com>
2025-05-20 20:03:36 +0000

    tabla hechos de est_jug_part hecha

--- models/marts/core/fct__estadisticas_jugador_partido.sql
+++ models/marts/core/fct__estadisticas_jugador_partido.sql
@@ -1,16 +1,10 @@
 {{ config(
     materialized='incremental',
-    unique_key='id_partido'
+    unique_key='id_partido_estadisticas'
 ) }}
 
 WITH partidos AS (
-    SELECT * 
-    FROM {{ ref('stg_atp_db__partidos') }}
-),
-    FROM {{ ref("stg_atp_db__estadisticas_partido_jug")}}
-
-renamed_casted AS (
-    SELECT
+    SELECT 
         id_partido,
         id_torneo_anio,
         id_ronda_torneo,
@@ -21,16 +15,47 @@ renamed_casted AS (
         sets_maximos,
         numero_partido_torneo,
         ingesta_tmz
-        
-        -- AÑADIR MACRO
+    FROM {{ ref('stg_atp_db__partidos') }}
 
-    FROM partidos
+),
 
-    -- INNER JOIN 
+estadisticas AS (
+    SELECT *
+    FROM {{ ref('stg_atp_db__estadisticas_partido_jug') }}
+),
+
+partido_jugador AS (
+    SELECT 
+        e.id_partido_estadisticas,
+        e.id_partido,
+        e.id_jugador,
+        e.ha_ganado,
+        e.aces,
+        e.dobles_faltas,
+        e.puntos_saque,
+        e.primeros_saques,
+        e.puntos_ganados_1er,
+        e.puntos_ganados_2do,
+        e.juegos_saque,
+        e.bp_salvados,
+        e.bp_enfrentados,
+        p.id_torneo_anio,
+        p.id_ronda_torneo,
+        p.id_ganador,
+        p.id_perdedor,
+        p.duracion_minutos,
+        p.resultado,
+        p.sets_maximos,
+        p.numero_partido_torneo,
+        {{ aniadir_campos_resultado('p.resultado') }},
+        p.ingesta_tmz
+    FROM estadisticas e
+    LEFT JOIN partidos p
+        ON e.id_partido = p.id_partido
 
     {% if is_incremental() %}
-      WHERE s.ingesta_tmz > (SELECT MAX(ingesta_tmz) FROM {{ this }})
+      WHERE ingesta_tmz > (SELECT MAX(ingesta_tmz) FROM {{ this }})
     {% endif %}
 )
 
-SELECT * FROM renamed_casted
+SELECT * FROM partido_jugador
--- models/staging/atp_db_matches/stg_atp_db__partidos.sql
+++ models/staging/atp_db_matches/stg_atp_db__partidos.sql
@@ -3,7 +3,7 @@
     unique_key='id_partido'
 ) }}
 
-WITH source AS (
+WITH partidos AS (
     SELECT 
         id_partido,
         id_torneo_anio,
@@ -16,43 +16,10 @@ WITH source AS (
         numero_partido_torneo,
         ingesta_tmz
 
-        --HACER MACROO
-
-        -- Número de sets jugados (coincidencias tipo 6-4, 7-6, etc.)
-        --REGEXP_COUNT(score, '\d+-\d+') AS sets_jugados,
-        -- Sets ganados por el ganador
-        --REGEXP_COUNT(score, '(^|\s)6-|(^|\s)7-') AS sets_ganador,
-        -- Sets ganados por el perdedor: total sets menos sets del ganador
-        --(REGEXP_COUNT(score, '\d+-\d+') - REGEXP_COUNT(score, '(^|\s)6-|(^|\s)7-')) AS sets_perdedor,
-        -- Número de tiebreaks jugados (detecta paréntesis tipo 7-6(5))
-        --REGEXP_COUNT(score, '\(\d+\)') AS tiebreaks_jugados,
-        -- Si el último set fue un tiebreak
-        --CASE
-            --WHEN REGEXP_SUBSTR(score, '\(\d+\)\s*$', 1) IS NOT NULL THEN TRUE
-            --ELSE FALSE
-        --END AS fue_tiebreak_final,
-        -- Resultado normalizado: separa sets con barra
-        --REPLACE(score, ' ', ' / ') AS resultado_normalizado,
     FROM {{ ref('base_atp_db__matches') }}
-),
-
-mapping AS (
-    SELECT *
-    FROM {{ ref('int__union_ids') }}
-),
-
-partidos AS (
-    SELECT
-         s.*  --,
-        -- m.ganador,
-        -- m.id_player1,
-        -- m.id_player2
-    FROM source s
-    LEFT JOIN mapping m
-      ON s.id_partido = m.id_partido
     {% if is_incremental() %}
       WHERE s.ingesta_tmz > (SELECT MAX(ingesta_tmz) FROM {{ this }})
     {% endif %}
-)
+),
 
 SELECT * FROM partidos
